c9ded2288a738787feeb247f9592b4e8cd2890d0
marine-vdb <marinewambergue@gmail.com>
2021-03-03 19:32:39 +0100

    Ajout de l'exercice "Addition"

---
+++ add.js
@@ -0,0 +1,6 @@
+window.onload = () => {
+        let a = Number(prompt("Veuillez saisir un nombre"));
+        let b = Number(prompt("Veuillez saisir un autre nombre"));
+        alert("Le resultat de l'addition est " + (a+b));
+}
+
